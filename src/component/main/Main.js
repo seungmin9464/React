c4021ff18dd3980a2966ef4a30962426533869e1
seungmin <backseungmin92@gmail.com>
2022-04-22 22:58:27 +0900

    Revert "no message"
    This reverts commit fc043fd503dbb4fb421005beb3e0edc9fd231196.

--- src/component/main/Main.js
+++ src/component/main/Main.js
@@ -1,47 +1,24 @@
 import style from './Main.module.scss'
+
+import './include/bubble'
 import Header from "./include/Header";
 
-import Typewriter from 'typewriter-effect';
-
 const Main = () => {
-    
     return (
         <div>
-            {/* header */}
             <Header/>
+            <section >
+                <canvas id="bubble"></canvas>
+            </section>
+            <section>
 
-            {/* Main */}
-            <div className={ style.section } >
-                <div className={ style.portfolio }>
-                    PORT<br/>FOLIO
-                    <div></div>
-                </div>
-                <div className={ style.typing }>
-                    <p>Hello, I'm</p>
-                    <p>
-                    <Typewriter
-                        options={{
-                            strings: ['Seungmin', 'Publisher', 'Developer'],
-                            autoStart: true,
-                            loop: true,
-                        }}
-                    />
-                    </p>
-                </div>
-            </div>
+            </section>
+            <section>
 
+            </section>
+            <section>
 
-            <div className={ style.section } >
-
-            </div>
-
-            <div className={ style.section } >
-
-            </div>
-            
-            <div className={ style.section } >
-
-            </div>
+            </section>
         </div>
     );
 };
