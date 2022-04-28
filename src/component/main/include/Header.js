39fe31926c019af37bf51f01f1da3c0dba197c61
https://github.com/seungmin9464/React.git <https://github.com/seungmin9464/React.git>
2022-04-28 23:05:42 +0900

    - mobile nav - mobile nav click - nav close

--- src/component/main/include/Header.js
+++ src/component/main/include/Header.js
@@ -1,5 +1,8 @@
 import { HeaderDiv, Logo, MenuList, MenuItem } from './style'
+import { FaBars, FaTimes } from 'react-icons/fa'
 import { Link } from 'react-scroll'
+import { useState } from 'react';
+import style from '../Main.module.scss'
 
 const headerMenu = [
     { id: '1', name: 'MAIN' },
@@ -9,13 +12,24 @@ const headerMenu = [
 ]
 
 const Header = ({ theme }) => {
+
+    const [click, setClick] = useState( false )
+    const handleClick = () => setClick(!click)
+
+    const closeMenu = () => setClick(false)
+
     return (
         <HeaderDiv>
             <Logo>
                 LOGO
             </Logo>
 
-            <MenuList theme={theme}>
+            <div className={style.hamburger} onClick={handleClick}>
+                {click ? (<FaTimes size={25} style={{ color: '#ffffff' }} />)
+                    : (<FaBars size={25} style={{ color: '#ffffff' }} />)}
+            </div>
+
+            <MenuList theme={ theme } className={click ? 'active' : ''}>
                 {
                     headerMenu.map( item =>
                         <Link
@@ -24,7 +38,10 @@ const Header = ({ theme }) => {
                             smooth = { true }
                             duration = { 500 }
                         >
-                            <MenuItem key={ item.id }>
+                            <MenuItem 
+                                key={ item.id }
+                                onClick={closeMenu}
+                            >
                                 { item.name }
                             </MenuItem>
                         </Link>
--- src/component/main/include/style.js
+++ src/component/main/include/style.js
@@ -11,6 +11,9 @@ export const HeaderDiv = styled.div`
     justify-content: space-between;
     opacity: .5;
     z-index: 999;
+    @media ${(props) => props.theme.mobile}{
+        opacity: 1;
+    }
 `
 
 export const Logo = styled.div`
@@ -30,10 +33,32 @@ export const MenuList = styled.ul`
     height: 100%;
     display: flex;
     align-items: center;
+    transition: ease 1s;
     @media ${(props) => props.theme.mobile}{
-        display: none;   
+        display: none;
+        left: 0;
+        &.active{
+            position: fixed;
+            display: flex;
+            top: 0px;
+            width: 100%;
+            height: 100%;
+            right: 100%;
+            background-color: #000;
+            flex-direction: column;
+            z-index: -10;
+            opacity: .7;
+            & a{
+                height: 100px;
+                width: 100%;
+                &:nth-child(1){
+                    padding-top: 80px;
+                }
+            }
+        }
     }
 `
+
 export const MenuItem = styled.li`
     height: 100%;
     display: flex;
