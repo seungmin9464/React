8dfaf6f89eb9d3eb625fb504cd9602119b5e0337
seungmin9464 <93364004+seungmin9464@users.noreply.github.com>
2025-01-20 10:07:21 +0000

    add new sentence

--- src/component/main/include/first/FirstSection.js
+++ src/component/main/include/first/FirstSection.js
@@ -15,7 +15,7 @@ const FirstSection = ({ chanege }) => {
                 <TypingText chanege={chanege}>
                     <Typewriter
                         options={{
-                            strings: ['Seungmin', 'Publisher', 'Developer123'],
+                            strings: ['Seungmin', 'Publisher', 'Developer111'],
                             autoStart: true,
                             loop: true,
                         }}
